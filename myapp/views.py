ea1e197a0cfdef84e4a695370d2a4bb643cb4382
vinay-gurram22 <gurramvinay1436@gmail.com>
2025-11-20 17:57:36 +0530

    created first basic html

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    items = Item.objects.all()
+    return render(request,"myapp/index.html",{"items":items})
+
+def detail(request,id):
+    item_one =Item.objects.get(id=id)
+    return render()
